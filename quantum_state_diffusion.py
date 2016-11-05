70c645aad14e3e527bd709fcc6782798d8d407e5
Gil Tabak <tabak.gil@gmail.com>
2016-11-05 14:53:05 -0700

    added expectation values of observables

--- quantum_state_diffusion.py
+++ quantum_state_diffusion.py
@@ -14,6 +14,9 @@ import sdeint
 from scipy import sparse
 import numpy.linalg as la
 from time import time
+import matplotlib as mil
+mil.use('TkAgg')
+import matplotlib.pyplot as plt
 
 
 def qsd_solve(H, psi0, tspan, Ls, sdeint_method, obsq = None, normalized = True, ntraj=1,):
@@ -30,7 +33,7 @@ def qsd_solve(H, psi0, tspan, Ls, sdeint_method, obsq = None, normalized = True,
         sdeint_method (Optional) SDE solver method:
             Which SDE solver to use. Default is sdeint.itoSRI2.
         obsq (optional): list of NxN csr matrices, dtype = complex128
-            Observables for which to generate trajectory information (TODO)
+            Observables for which to generate trajectory information
         normalized (optional): Boolean
             Use the normalized quantum state diffusion equations. (TODO: case False)
         ntraj (optional): int
@@ -82,70 +85,28 @@ def qsd_solve(H, psi0, tspan, Ls, sdeint_method, obsq = None, normalized = True,
         raise ValueError("Case normalized == False is not implemented.")
 
     psi0_arr = np.asarray(psi0.todense()).T[0]
-    result = sdeint_method(f,G,psi0_arr,tspan)
+    psis = sdeint_method(f,G,psi0_arr,tspan)
 
-    return result
+    ## maybe there is a more efficient way to do this...
+    obsq_expects = [ np.asarray([ob.dot(psi).dot(psi.conj()) for psi in psis])
+                        for ob in obsq ]
+
+    return psis, obsq_expects
 
 if __name__ == "__main__":
 
     psi0 = sparse.csr_matrix(([0,0,0,0,0,0,0,1.]),dtype=np.complex128).T
-    H = sparse.csr_matrix(np.diag([1,1,1,1,1,1,1,1]),dtype=np.complex128)
-    Ls = [sparse.csr_matrix( [[0,1,0,0,0,0,0,0],
-                                  [0,0,np.sqrt(2),0,0,0,0,0],
-                                  [0,0,0,np.sqrt(3),0,0,0,0],
-                                  [0,0,0,0,np.sqrt(4),0,0,0],
-                                  [0,0,0,0,0,np.sqrt(5),0,0],
-                                  [0,0,0,0,0,0,np.sqrt(6),0],
-                                  [0,0,0,0,0,0,0,np.sqrt(7)],
-                                  [0,0,0,0,0,0,0,0]],dtype=np.complex128)]
+    H = sparse.csr_matrix(np.eye(8),dtype=np.complex128)
+    Ls = [sparse.csr_matrix( np.diag([np.sqrt(i) for i in range(1,8)],k=1),dtype=np.complex128)]
     tspan = np.linspace(0,0.5,5000)
+    obsq = [sparse.csr_matrix(np.diag([i for i in range(4)]*2),dtype=np.complex128)]
 
     T_init = time()
-    output = qsd_solve(H, psi0, tspan, Ls, sdeint.itoSRI2)
+    psis,obsq_expects = qsd_solve(H, psi0, tspan, Ls, sdeint.itoSRI2, obsq = obsq)
     T_fin = time()
 
     print ("time to run:  ", T_fin - T_init, " seconds.")
-
-    print ("Last point: ",output[-1])
-    print ("Norm of last point: ",la.norm(output[-1]))  ## should be close to 1...
-
-
-###### Deprecated, but could still be useful
-
-    # def doubled_to_complex(psi2):
-    #     '''
-    #     Convertes from:
-    #         <(2N)xM sparse matrix of type '<class 'numpy.float64'>'
-    #             in Compressed Sparse Row format>
-    #     to:
-    #         <NxM sparse matrix of type '<class 'numpy.complex128'>'
-    #     	    in Compressed Sparse Row format>
-    #
-    #     This function ASSUMES that the row dimension of the input is 2N
-    #     (specifically, even).
-    #     '''
-    #     return psi2[:N,:] + 1j*psi2[N:,:]
-    #
-    # def complex_to_doubled(psi):
-    #     '''
-    #     Convertes from:
-    #         <NxM sparse matrix of type '<class 'numpy.complex128'>'
-	#            in Compressed Sparse Row format>
-    #     to:
-    #         <(2N)xM sparse matrix of type '<class 'numpy.float64'>'
-    #            in Compressed Sparse Row format>
-    #
-    #     CSR format is chosen for faster splitting along the rows.
-    #     '''
-    #     return sparse.vstack([psi.real,psi.imag],format = 'csr')
-    #
-    # def generate_sdeint_functions(complex_func):
-    #     def arr_func(psi_arr,t):
-    #         psi_as_doubled_sparse_matix = sparse.csr_matrix(psi_arr).T
-    #         psi_as_complex_sparse_matix = doubled_to_complex(psi_as_doubled_sparse_matix)
-    #         complex_func_val = complex_func(psi_as_complex_sparse_matix,t)
-    #         doubled_func_val = complex_to_doubled(complex_func_val)
-    #         dense_func_val = doubled_func_val.todense()
-    #         arr_func_val = np.asarray(dense_func_val).T[0]
-    #         return arr_func_val
-    #     return arr_func
+    print ("Last point: ",psis[-1])
+    print ("Norm of last point: ",la.norm(psis[-1]))  ## should be close to 1...
+    plt.plot(tspan,obsq_expects[0])
+    plt.show()
