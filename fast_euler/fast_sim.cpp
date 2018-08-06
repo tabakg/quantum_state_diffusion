42d1223e2aa159d13bf813cded4a1fc82d9510b1
Gil Tabak <tabak.gil@gmail.com>
2018-08-05 18:35:29 -0700

    updated simulation code

--- fast_euler/fast_sim.cpp
+++ fast_euler/fast_sim.cpp
@@ -1,7 +1,7 @@
 
 /*
-g++ open_file.cpp -o open_file -std=c++11
-./open_file
+g++ fast_sim.cpp -o fast_sim -std=c++11
+./fast_sim
 */
 
 // basic file operations
@@ -12,11 +12,14 @@ g++ open_file.cpp -o open_file -std=c++11
 #include <random>
 #include <thread>
 #include <math.h>       /* sqrt */
+#include <assert.h>     /* assert */
+#include <chrono>
 
 
 // for convenience
 using json = nlohmann::json;
 using namespace Eigen;
+using namespace std::chrono;
 
 typedef std::string string;
 typedef std::complex<float> comp;
@@ -52,21 +55,10 @@ std::vector<string> inputs_two_systems = {"H1_eff",
 
 typedef struct
 {
-  // H_eff
-  std::vector<comp_vec> H_eff_diagonals;
-  std::vector<comp_vec> H_eff_diags_x_psi;
-  std::vector<int> H_eff_offsets;
-
-  // Ls
-  std::vector<std::vector<comp_vec>> Ls_diagonals;
-  std::vector<std::vector<int>> Ls_offsets;
-
-  // obsq
-  std::vector<std::vector<comp_vec>> obsq_diagonals;
-  std::vector<std::vector<int>> obsq_offsets;
 
   // psi0
   comp_vec psi0;
+  int dimension;
 
   // Other parameters
   float duration;
@@ -74,6 +66,21 @@ typedef struct
   string sdeint_method;
   int downsample;
   int ntraj;
+
+  // H_eff
+  std::vector<comp_vec> H_eff_diagonals;
+  std::vector<int> H_eff_offsets;
+  std::vector<comp_vec> H_eff_diags_x_psi;
+
+  // Ls
+  std::vector<std::vector<comp_vec>> Ls_diagonals;
+  std::vector<std::vector<int>> Ls_offsets;
+  std::vector<std::vector<comp_vec>> Ls_diags_x_psi;
+  std::vector<comp> Ls_expectations;
+
+  // obsq
+  std::vector<std::vector<comp_vec>> obsq_diagonals;
+  std::vector<std::vector<int>> obsq_offsets;
 
 } one_system;
 
@@ -123,7 +130,7 @@ int get_num_systems(json & j){
   else return -1;
 }
 
-comp_vec json_to_complex_array(json & j_arr, float scaling=1.){
+comp_vec json_to_complex_array(json & j_arr, float scaling = 1.){
   // Assumes a json serialization with fields "real" and "imag".
   // These should be arrays of the same size containing floats.
   // Output is a std::vector with complex<float> entries.
@@ -142,11 +149,12 @@ comp_vec json_to_complex_array(json & j_arr, float scaling=1.){
 
 void load_diag_operator(std::vector<comp_vec> & diagonals,
                         std::vector<int> & offsets,
-                        json & diag_json){
+                        json & diag_json,
+                        float scaling=1.){
   // Loads from JSON with fields "data" and "offsets" representing a sparse matrix.
   json json_data = diag_json["data"];
   for (int i = 0; i < json_data.size(); i++){
-    diagonals.push_back(json_to_complex_array(json_data[i]));
+    diagonals.push_back(json_to_complex_array(json_data[i], scaling=scaling));
   }
   json json_offsets = diag_json["offsets"];
   for (int i = 0; i < json_offsets.size(); i++){
@@ -156,12 +164,13 @@ void load_diag_operator(std::vector<comp_vec> & diagonals,
 
 void load_diag_operator_sequence(std::vector<std::vector<comp_vec>> & diagonals,
                         std::vector<std::vector<int>> & offsets,
-                        json & diag_json){
+                        json & diag_json,
+                        float scaling=1.){
   // Load each set of diagonals and offsets for each in the sequence
   std::vector<comp_vec> diags_entry;
   std::vector<int> offsets_entry;
   for (int i = 0; i < diag_json.size(); i++){
-    load_diag_operator(diags_entry, offsets_entry, diag_json[i]);
+    load_diag_operator(diags_entry, offsets_entry, diag_json[i], scaling=scaling);
     diagonals.push_back(diags_entry);
     offsets.push_back(offsets_entry);
   }
@@ -187,6 +196,18 @@ void add_second_to_first(comp_vec& in_1, comp_vec& in_2){
   }
 }
 
+void add_second_to_first(comp_vec& in_1, comp_vec& in_2, comp scalar){
+  // Adds second to first
+  int size = in_1.size();
+  for (int i=0; i<size; i++){
+    in_1[i] += in_2[i] * scalar;
+  }
+}
+
+void mult_vecs(comp_vec& v1, comp_vec& v2, comp_vec& out){
+  transform(v1.begin(), v1.end(), v2.begin(), out.begin(), std::multiplies<comp>() );
+}
+
 void mult_vecs_offset_upper(comp_vec& diag, comp_vec& vec, comp_vec& out, int& offset){
   /* Multiplies arrays with an offset.
 
@@ -207,51 +228,134 @@ void mult_vecs_offset_lower(comp_vec& diag, comp_vec& vec, comp_vec& out, int& o
   transform(diag.begin(), diag.end(), vec.begin(), out.begin()+offset, std::multiplies<comp>() );
 }
 
-void normalize (std::vector<comp> & vec){
-  float total = 0;
-  int size = vec.size();
-  for(int i=0; i<size; i++){
-    total += std::norm(vec[i]);
+comp dot(comp z1, comp z2){
+  // returns z1.conj() * z2
+  float a, b, c, d;
+  a = real(z1); b = imag(z1);
+  c = real(z2); d = imag(z2);
+  return comp(a * c + b * d, a * d - b * c);
+}
+
+void dot_vecs(comp_vec& v1, comp_vec& v2, comp & val){
+  for (int i=0; i<v1.size(); i++){
+    val += dot(v1[i], v2[i]);
   }
-  for(int i=0; i<size; i++){
+}
+
+comp norm(std::vector<comp> & vec){
+  comp val = 0.;
+  dot_vecs(vec, vec, val);
+  return sqrt(val);
+}
+
+void normalize(std::vector<comp> & vec){
+  comp total = norm(vec);
+  for(int i=0; i<vec.size(); i++){
     vec[i] /= total;
   }
 }
 
+void normalize(std::vector<comp> & vec, comp total){
+  for(int i=0; i<vec.size(); i++){
+    vec[i] /= total;
+  }
+}
+
+void update_products(std::vector<int> & offsets,
+                     std::vector<comp_vec> & diags,
+                     comp_vec & current_psi,
+                     std::vector<comp_vec> & products){
+  for (int i=0; i<offsets.size(); i++){
+    int offset = offsets[i];
+    if (offset >= 0){
+      mult_vecs_offset_upper(diags[i],
+                             current_psi,
+                             products[i],
+                             offset);
+    }
+    else{
+      offset *= -1;
+      mult_vecs_offset_lower(diags[i],
+                             current_psi,
+                             products[i],
+                             offset);
+    }
+  }
+}
+
+void update_products_sequence(std::vector<std::vector<int>> & offsets,
+                              std::vector<std::vector<comp_vec>> & diags,
+                              comp_vec & current_psi,
+                              std::vector<std::vector<comp_vec>> & products){
+  for (int i=0; i<offsets.size(); i++){
+    update_products(offsets[i], diags[i], current_psi, products[i]);
+  }
+}
+
+comp expectation_from_vecs(std::vector<comp_vec> & product,
+                            comp_vec & current_psi){
+  comp expectation = 0.;
+  for (int i=0; i<product.size(); i++){
+    dot_vecs(current_psi, product[i], expectation);
+  }
+  return expectation;
+}
+
+void update_Ls_expectations(comp_vec & current_psi,
+                            std::vector<std::vector<comp_vec>> & products,
+                            std::vector<comp> & expectations){
+  for (int i=0; i<products.size(); i++){
+    expectations[i] = expectation_from_vecs(products[i], current_psi);
+  }
+}
 
 void take_step(one_system & system, std::vector<comp> & noise, std::vector<comp> & current_psi){
 
   // Update structures
   // TODO: separate data for positive and negative offsets
-  for (int i=0; i<system.H_eff_offsets.size(); i++){
-    int offset = system.H_eff_offsets[i];
-    if (offset >= 0){
-      mult_vecs_offset_upper(system.H_eff_diagonals[i],
-                             current_psi,
-                             system.H_eff_diags_x_psi[i],
-                             offset);
-    }
-    else{
-      offset *= -1;
-      mult_vecs_offset_lower(system.H_eff_diagonals[i],
-                                current_psi,
-                                system.H_eff_diags_x_psi[i],
-                                offset);
-    }
-  }
+  update_products(system.H_eff_offsets, system.H_eff_diagonals, current_psi, system.H_eff_diags_x_psi);
+  update_products_sequence(system.Ls_offsets, system.Ls_diagonals, current_psi, system.Ls_diags_x_psi);
+  update_Ls_expectations(current_psi, system.Ls_diags_x_psi, system.Ls_expectations);
 
   // update psi
   for (int i=0; i<system.H_eff_diags_x_psi.size(); i++){
     add_second_to_first(current_psi, system.H_eff_diags_x_psi[i]);
   }
+
+  for (int i=0; i<system.Ls_diags_x_psi.size(); i++){
+    comp mult_L_by = std::conj(system.Ls_expectations[i]) + noise[i];
+    for (int j=0; j<system.Ls_diags_x_psi[i].size(); j++){
+      add_second_to_first(current_psi, system.Ls_diags_x_psi[i][j], mult_L_by);
+    }
+  }
+
+
+
   // normalize psi
-  normalize(current_psi);
   //TODO: monitor size of psi (when to normalize)
+  normalize(current_psi);
+}
+
+void show_state(comp_vec current_psi, int dimension){
+
+  // clear screen magic
+  std::cout << "\033[2J\033[1;1H";
+
+  for (int i=0; i<dimension; i++){
+    std::cout << i << "  ";
+    if (i < 10)
+      std::cout << " ";
+    int num_stars = int(norm(current_psi[i]) * 100);
+    for (int j=0; j<num_stars; j++){
+      std::cout << "*";
+    }
+    std::cout << std::endl;
+  }
 }
 
 void run_trajectory(one_system system, int seed, int steps_for_noise){
   // Find dimension...
-  int dim = system.psi0.size();
+  system.dimension = system.psi0.size();
 
   // make generator for random numbers
   std::default_random_engine generator(seed);
@@ -264,40 +368,44 @@ void run_trajectory(one_system system, int seed, int steps_for_noise){
   int num_downsampled_steps = int(num_steps / system.downsample);
 
   // accepts float, double, or long double
-  std::normal_distribution<float> distribution(0., sqrt(system.delta_t/2.));
+  float mean = 0;
+  float std_dev = sqrt(0.5 * system.delta_t);
+  std::normal_distribution<float> distribution(0., std_dev);
 
   // random variables stored here. They are replenished every steps_for_noise steps.
   std::vector<std::vector<comp>> randoms(steps_for_noise, std::vector<comp>(num_noise));
 
   // Initialize downsampled states.
-  std::vector<std::vector<comp>> psis(num_downsampled_steps, std::vector<comp>(dim));
+  std::vector<std::vector<comp>> psis(num_downsampled_steps, std::vector<comp>(system.dimension));
 
   std::vector<comp> current_psi = system.psi0;
 
   //////// Main for loop for simulation
-  for(int i=0, j=steps_for_noise, k=system.downsample, l=0; i<num_steps; i++, j--, k--){
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+  for(int i=0, j=0, k=0, l=0; i<num_steps; i++, j++, k++){
     // Get new noise terms as we go
-    if (j == steps_for_noise){
+    if (j == steps_for_noise)
+      j = 0;
+    if (k == system.downsample)
+      k = 0;
+    if (j == 0)
       get_new_randoms(randoms, steps_for_noise, num_noise, generator, distribution);
-    }
-    else if (j == 0){
-      j = steps_for_noise;
-    }
-    if (k == system.downsample){
-      psis[l] = current_psi;
-      l++; // number of psis recorded
-    }
-    else if (k == 0){
-      k = system.downsample;
+    if (k == 0){
+      // std::copy(current_psi.begin(), current_psi.end(), psis[l].begin());
+      // l++; // number of psis recorded
+      // show_state(current_psi, system.dimension);
     }
     take_step(system, randoms[j], current_psi);
   }
 
-  // Print state after simulation.
-  for (int i=0; i<50; i++){
-    std::cout << current_psi[i] << std::endl;
-  }
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  duration<double> time_span = duration_cast< duration<double> >(t2 - t1);
 
+  show_state(current_psi, system.dimension);
+
+  std::cout << "It took me " << time_span.count() << " seconds." << std::endl;
+  std::cout << "Time per step was: " << time_span.count() / num_steps * 1000000 << " micro seconds." << std::endl;
 }
 
 void qsd_one_system(json & j, int steps_for_noise = 10000){
@@ -307,16 +415,11 @@ void qsd_one_system(json & j, int steps_for_noise = 10000){
 
   one_system system;
 
-  // Loading various operators as diagonals
-  // TODO: scale operators by delta_t (and then noise terms too!)
-  load_diag_operator(system.H_eff_diagonals, system.H_eff_offsets, j["H_eff"]);
-  load_diag_operator_sequence(system.Ls_diagonals, system.Ls_offsets, j["Ls"]);
-  load_diag_operator_sequence(system.obsq_diagonals, system.obsq_offsets, j["obsq"]);
-
   // Other items to retrieve from JSON
 
   // psi0
   system.psi0 = json_to_complex_array(j["psi0"]);
+  system.dimension = system.psi0.size();
 
   // Other parameters
   system.duration = j["duration"];
@@ -324,10 +427,24 @@ void qsd_one_system(json & j, int steps_for_noise = 10000){
   system.sdeint_method = j["sdeint_method"];
   system.downsample = j["downsample"];
   system.ntraj = j["ntraj"];
+  assert(j["seeds"].size() == system.ntraj);
+
+  // Loading various operators as diagonals
+  // TODO: scale operators by delta_t (and then noise terms too!)
+  load_diag_operator(system.H_eff_diagonals, system.H_eff_offsets, j["H_eff"], system.delta_t);
+  load_diag_operator_sequence(system.Ls_diagonals, system.Ls_offsets, j["Ls"], sqrt(system.delta_t));
+  load_diag_operator_sequence(system.obsq_diagonals, system.obsq_offsets, j["obsq"]);
 
   // Initialize other objects useful in the simulation.
-  system.H_eff_diags_x_psi = std::vector<std::vector<comp>>(system.H_eff_diagonals.size(),
-                             std::vector<comp>(system.psi0.size()));
+  system.H_eff_diags_x_psi = std::vector<comp_vec>(system.H_eff_diagonals.size(),
+                             std::vector<comp>(system.dimension));
+
+  system.Ls_diags_x_psi = std::vector<std::vector<comp_vec>>(system.Ls_diagonals.size());
+  for (int i=0; i<system.Ls_diagonals.size(); i++){
+    system.Ls_diags_x_psi[i] = std::vector<comp_vec>(system.Ls_diagonals[i].size(),
+                               std::vector<comp>(system.dimension));
+  }
+  system.Ls_expectations = std::vector<comp>(system.Ls_diagonals.size());
 
   //////// Run trajectories
 
